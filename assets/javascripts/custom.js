ee48ba4c90fe7120b5302da5fb3124ceed6ec1c5
Matt Walter <mattwalter91@gmail.com>
2018-08-27 09:59:43 -0500

    location

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -3,7 +3,7 @@ $(function(){
 });
 
 function setTime(){
-	let location = "Playa Del Carmen, Mexico";
+	let location = "Isla Mujeres, Mexico";
 	let tz = "America/Cancun";
 	var time = moment().tz(tz).format('h:mm a');
 	$('#location').html('Current Location: '+location+' - '+time);
